e432b12f1c1033b9c19e29144e099330fa0073c2
Mac1618 <121495949+Mac1618@users.noreply.github.com>
2023-05-07 17:37:02 +0800

    Update FileUploadController.js

--- Controller/FileUpload/FileUploadController.js
+++ Controller/FileUpload/FileUploadController.js
@@ -112,7 +112,7 @@ const getFile = async (req, res) => {
         // logs all the Data inside FileUploadModel
         const files = await FileUploadModel.findById({ _id: id });
         const paths = __dirname;
-        const downloadPath = path.join(paths,'\\', files.path).replace('Controller/FileUpload\\\\', '');
+        const downloadPath = path.join(paths,'\\', files.path).replace('Controller/FileUpload/\\', '');
 
         res.set({
             'Content-Type': files.mimetype
