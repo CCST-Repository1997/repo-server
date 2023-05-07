a84d6e3638751cfdc912d6eeb6dd2f79b13af6ea
Mac1618 <121495949+Mac1618@users.noreply.github.com>
2023-05-07 22:02:40 +0800

    Update StudentTaskController.js

--- Controller/FileUpload/StudentTaskController.js
+++ Controller/FileUpload/StudentTaskController.js
@@ -181,7 +181,6 @@ const deleteStudentTask = async (req, res) => {
         //delete the file
             //path
             const paths = __dirname;
-            const downloadPath = path.join(paths,'ManuscriptFiles/')
             const downloadPath = path.join(paths,'ManuscriptFiles/').replace('Controller/FileUpload/', '');
         
             //filename
